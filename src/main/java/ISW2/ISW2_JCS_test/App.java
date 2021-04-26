c5781e03a374039d473c35743086cddf7108bcda
pierciro <piercirocaliandro998@gmail.com>
2021-04-26 09:55:12 +0200

    initial

---
+++ src/main/java/ISW2/ISW2_JCS_test/App.java
@@ -0,0 +1,13 @@
+package ISW2.ISW2_JCS_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
